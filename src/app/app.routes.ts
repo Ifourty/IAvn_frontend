810ad6b5b8d5c44698b8d8760f1d394e59c714e0
Enzo <fireredsark@gmail.com>
2025-03-25 16:18:31 +0100

    Feat : home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { NewGamePageComponent } from './newGame/new-game-page/new-game-page.component';
+import { NotFoundPageComponent } from './notFound/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    },
+    {
+        path: 'newGame',
+        component: NewGamePageComponent
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent
+    }
+]
---
+++ src/app/newGame/new-game-page/new-game-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-game-page',
+  imports: [],
+  templateUrl: './new-game-page.component.html',
+  styleUrl: './new-game-page.component.scss'
+})
+export class NewGamePageComponent {
+
+}
